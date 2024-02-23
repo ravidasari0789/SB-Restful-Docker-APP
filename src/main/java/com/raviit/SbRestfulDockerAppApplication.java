fd2689eca2d6ffb4f70a532172dbd9a835125634
ravidasari0789 <ravidasari0789@gmail.com>
2024-02-23 14:33:44 +0530

    Spring boot restfull docker file application commited

---
+++ src/main/java/com/raviit/SbRestfulDockerAppApplication.java
@@ -0,0 +1,13 @@
+package com.raviit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRestfulDockerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbRestfulDockerAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/raviit/controller/DockerController.java
@@ -0,0 +1,16 @@
+package com.raviit.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+
+	
+	              @GetMapping("/docker")
+	              public String messageHello() {
+	            	 
+	            	  String msg="Welocme to Docker Container";
+	            	  return msg;
+	              }
+}
